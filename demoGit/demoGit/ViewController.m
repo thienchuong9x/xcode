80cdeb76672f083ef746fc8d01071d81799ae815
thienchuong9x <thienchuong.la91@gmail.com>
2014-09-29 20:51:48 -0700

    testcommit

---
+++ demoGit/demoGit/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  demoGit
+//
+//  Created by chuongntt on 9/29/14.
+//  Copyright (c) 2014 chuongntt. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
